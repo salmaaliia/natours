9af90c1fb9a8bcdd9d4240bb106adfae51aa66ea
Salma <salmaabdelmotagaly390@gmail.com>
2025-10-02 16:28:16 +0300

    fix typo error

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,7 +4,7 @@ const authController = require('../controllers/authController');
 const bookingController = require('../controllers/bookingController');
 const router = express.Router();
 
-router.use(viewsController.alerts);
+router.use(viewsController.alert);
 
 router.get('/me', authController.protect, viewsController.getAccount);
 router.get(
